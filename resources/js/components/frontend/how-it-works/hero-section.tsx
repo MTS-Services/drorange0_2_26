29ef6ffe953ece43ff19e438e51af28fd84a2a45
dev-sumon <akhtaruzzamansumon7@gmail.com>
2026-03-25 12:03:22 +0600

    How It Works pages update

--- resources/js/components/frontend/how-it-works/hero-section.tsx
+++ resources/js/components/frontend/how-it-works/hero-section.tsx
@@ -5,8 +5,8 @@ export function HeroSection({banner}: any) {
 
     return (
       <div className="text-center bg-[#193CB8] lg:p-24 p-12">
-      <h2 className="lg:text-5xl text-3xl font-black text-white mb-3 font-arial font-medium ">{banner.title }</h2>
-      <p className="text-gray-100 lg:text-lg text-base font-medium  max-w-2xl mx-auto leading-relaxed">
+      <h2 className="text-3xl lg:text-5xl font-inter font-normal text-white mb-3">{banner.title }</h2>
+      <p className="text-gray-100 lg:text-lg text-base font-normal  font-inter max-w-2xl mx-auto leading-relaxed">
         {banner.subtitle}
       </p>
       <div className="max-w-2xl mx-auto ">
--- resources/js/components/frontend/how-it-works/stay-informed-section.tsx
+++ resources/js/components/frontend/how-it-works/stay-informed-section.tsx
@@ -5,38 +5,37 @@ interface StayInform {
     subtitle: string;
 }
 export function StayInformedSection({ stayInforms }: { stayInforms: StayInform[] }) {
-   
+
     return (
         <section className="bg-white py-8 lg:py-16">
             <div className="mx-auto max-w-7xl px-4">
                 <div className="mb-10 text-center">
-                    <h2 className="mb-2 text-2xl font-black font-bold text-gray-900 lg:text-3xl">
+                    <h2 className="mb-2 text-[32px] font-bold text-gray-900 font-inter">
                         Stay Informed Every Step
                     </h2>
-                    <p className="mx-auto max-w-xl text-sm text-gray-500">
-                        Our platform keeps you updated throughout your bathroom
-                        renovation.
+                    <p className="text-xl font-normal text-gray-500 font-inter">
+                        Our platform keeps you updated throughout your bathroom remodel
                     </p>
                 </div>
-                <div className="grid grid-cols-2 gap-4 md:grid-cols-4">
+                <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 md:grid-cols-4">
 
                     {
                         stayInforms?.map((stayInform: StayInform, index: number) => (
-                              <div className="inform-card px-4">
+                            <div className="inform-card px-4">
                                 <div className="inform-icon py-2">
-                                    <img src={stayInform.icon_url}  />
+                                    <img src={stayInform.icon_url} />
                                 </div>
-                                <h4 className="mb-1 text-xl font-medium text-gray-900">
+                                <h4 className="mb-1 text-xl font-normal text-gray-900 font-inter">
                                     {stayInform.title}
                                 </h4>
-                                <p className="text-xs leading-relaxed text-gray-500">
+                                <p className="text-base font-normal text-gray-500 font-inter">
                                     {stayInform.subtitle}
                                 </p>
                             </div>
-                            ) 
+                        )
                         )
                     }
-                  
+
 
                 </div>
             </div>
